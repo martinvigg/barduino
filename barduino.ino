8bfe127febb5a7b74b827637f7f15c08c7b1a978
martinvigg <martinvigg@outlook.com>
2015-09-24 22:59:21 -0300

    mejoras

--- barduino.ino
+++ barduino.ino
@@ -1,16 +1,62 @@
+#include <EEPROM.h>
 #include <Stepper.h>
 #define CAUDAL 1
 
-//enum bebidas {FERNET = 1, VODKA, RON, TEQUILA, COCA_COLA, };
+#define PIN_V0 4 // PIN VALVULA 0
+#define PIN_V1 5
+#define PIN_V2 6
+#define PIN_V3 7
+#define PIN_V4 0
+#define PIN_V5 0
+#define PIN_V6 0
+#define PIN_V7 0
+
+#define V0_EEPROM 10 // guardo las sucesivas en los espcios siguientes.
+
+#define STP1  9 // PINES DEL MOTOR PAP
+#define STP2 10
+#define STP3 11
+#define STP4 12
+
+#define NUM_VALVES 4
+
+#define PIN_FDC 8 //NORMAL ABIERTO DEL FINAL DE CARRERA DEL HOME
+#define POS_POR_PASO 1.0 //TODO: ver que pongo aca
+
+String bebidas_s[] = {"FERNET", "VODKA","RON", "TEQUILA", "COCA COLA", "NARANJA"};
+enum bebidas {FERNET = 0, VODKA, RON, TEQUILA, COCA_COLA, NARANJA};
+
+typedef struct valve_s {
+    byte index;
+    byte drink;
+    byte pin;
+    byte active;
+    long pos;
+} valve_t;
+
+valve_t valve[NUM_VALVES];
+
+Stepper stepper(400, STP1, STP2, STP3, STP4);
 
 char sepChar = ' ';
 char subSepChar = ';';
 String bufferString = "";
+long currentPos = 0;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  
+  stepper.setSpeed(100);
+  readEEPROM();
+  valve[0].pin = PIN_V0;
+  valve[1].pin = PIN_V1;
+  valve[2].pin = PIN_V2;
+  valve[3].pin = PIN_V3;
+  pinMode(PIN_V0, OUTPUT);
+  pinMode(PIN_V1, OUTPUT);
+  pinMode(PIN_V2, OUTPUT);
+  pinMode(PIN_V3, OUTPUT);
+  goHome(); // hago que el motor valla a home.
 }
 
 void loop() {
@@ -33,6 +79,18 @@ void serialEvent(){
   }  
 }
 
+void setUpValve (int index, int drinkCode){
+  valve[index].active = 1;
+  EEPROM.update(3*V0_EEPROM + index, 1);
+  valve[index].drink = drinkCode;
+  EEPROM.update(V0_EEPROM + index, drinkCode);  
+}
+
+void closeValve (int index){
+  valve[index].active = 0;
+  EEPROM.update(3*V0_EEPROM + index, 0);
+}
+
 void parse(String str) {
   int i;
   String arg[10] = {"","","","","","","","","",""};
@@ -49,6 +107,16 @@ void parse(String str) {
   cmdToAction(arg, numArg);  
 }
 
+void readEEPROM(){
+  int i;
+  for (i = 0; i < NUM_VALVES; i++){
+    valve[i].active = EEPROM.read(3*V0_EEPROM+i);
+    if (valve[i].active) {
+      valve[i].drink = EEPROM.read(V0_EEPROM + i);
+    }
+  }
+}
+
 void cmdToAction(String arg[],int numArg){
   int drinkArray [numArg-1][2];
   arg[0].toUpperCase();
@@ -56,41 +124,41 @@ void cmdToAction(String arg[],int numArg){
   
   //EJEMPLO DE BEBIDAS PERSONALIZADAS
   if (arg[0] == "MAKE"){ // MAKE volumen_del_vaso cod_bebida;porcentaje cod_bebida;porcentaje ...
-    //volumen = arg[1]
+    int volumen = round(arg[1].toInt() * 0.8); //multiplico por 0.8 para darme un poco de juego
     
     for (int i = 2; i <= numArg; i++) {        
       drinkArray[i-2][0] = arg[1].toInt(); //TODO: decidir si le paso las bebidas como numero o como string
-      drinkArray[i-2][1] = arg[2].toInt(); //TODO: conviene usar float?   
+      drinkArray[i-2][1] = floor(arg[2].toInt() * volumen / 100.0); //redondeo para abajo para asegurarme de que no me paso del volumen total 
     }
        
-    if (validateInput(drinkArray,numArg-2)) {
-      delay(1);// pasar a otra funcion
+    if (validateInput(volumen, drinkArray,numArg-2)) {
+      make(drinkArray, numArg -2);
     } else {
       delay(1); 
     }
   }
 
-  //EJEMPLO DE BEBIDAS PRE-ESTABLECIDAS
-  if (arg[0] == "FERNET"){ // FERNET volumen_del_vaso porcentaje
-    if (numArg == 2){//volumen = arg[1]
-      //int drinkArray [numArg-1][2];
-      drinkArray[0][0] = 11111; //codigo del fernet
-      drinkArray[0][1] = arg[2].toInt();
-      drinkArray[1][0] = 11111; //codigo de la coca
-      drinkArray[1][1] = 100 - arg[2].toInt();
-
-      if (validateInput(drinkArray,numArg-2)) {
-        delay(1);// pasar a otra funcion
-      } else {
-        delay(1); 
-      }
-    } else {
-      delay(1); //error
-    }   
-  }
+//EJEMPLO DE BEBIDAS PRE-ESTABLECIDAS
+//  if (arg[0] == "FERNET"){ // FERNET volumen_del_vaso porcentaje
+//    if (numArg == 2){//volumen = arg[1]
+//      //int drinkArray [numArg-1][2];
+//      drinkArray[0][0] = 11111; //codigo del fernet
+//      drinkArray[0][1] = arg[2].toInt();
+//      drinkArray[1][0] = 11111; //codigo de la coca
+//      drinkArray[1][1] = 100 - arg[2].toInt();
+//
+//      if (validateInput(drinkArray,numArg-2)) {
+//        delay(1);// pasar a otra funcion
+//      } else {
+//        delay(1); 
+//      }
+//    } else {
+//      delay(1); //error
+//    }   
+//  }
 }
 
-boolean validateInput(int d[][2], int dim){
+boolean validateInput(int v,int d[][2], int dim){
   int i;
   int porcentajeSum = 0;
 
@@ -98,15 +166,60 @@ boolean validateInput(int d[][2], int dim){
     porcentajeSum += d[i][1]; //TODO: validar los codigos de bebidas
   }
 
-  return (porcentajeSum == 100);  
+  return (porcentajeSum <= v);  
 }
 
-void pour(int valNum, int cant) {
+void pour(int pin, int cant) {
   long t;
   
   t = round((float)cant/CAUDAL); //TODO: ver tema de unidades.
 
-  digitalWrite(valNum, HIGH);
+  digitalWrite(pin, HIGH);
   delay(t);
-  digitalWrite(valNum, LOW);  
+  digitalWrite(pin, LOW);  
 }
+
+byte getValveIndexFromDrink(int d){
+  int i;
+  
+  for (i = 0; i < NUM_VALVES; i++){
+    if (valve[i].drink == d){
+      return i;
+    }
+  }
+  
+  return -1;
+}
+
+void make(int d[][2], int dim){
+  int i, v_i;
+  if (currentPos) goHome();
+
+  for (i = 0; i<dim; i++){
+      v_i = getValveIndexFromDrink(d[i][0]);
+      goTo(valve[v_i].pos);
+      pour(valve[v_i].pin, d[i][1]);
+  }
+
+  //TODO: revolver bebida ;) ;)
+  
+  goHome();  
+}
+
+long distanceToSteps(long pos) {
+  return round(pos/POS_POR_PASO);
+}
+
+void goTo(long pos){
+  stepper.step(distanceToSteps(currentPos - pos)); // diferencia entre la posición actual y la de destino.
+  currentPos = pos;                                // hay que configurar los cables del stepper para que step(1) valla hacia afuera
+                                                   // y step(-1) valla hacia adentro.
+}
+
+void goHome(){
+  while (digitalRead(PIN_FDC) == LOW){
+    stepper.step(-1);
+  }
+  currentPos = 0;
+}
+
